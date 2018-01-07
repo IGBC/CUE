f6b3f60472c1adb8f311419249f65930ddc962d3
SEGFAULT <fakespamtrap@gmail.com>
2018-01-07 04:47:16 +0100

    extending shell stream getters to stdin and stderr

--- src/shell.rs
+++ src/shell.rs
@@ -42,8 +42,20 @@ impl Shell {
         jobID
     }
     
+    // Stream Getters, seriously don't ask why this works.
+    
+    pub fn get_stdin(&mut self, job: usize) -> &mut ChildStdin {
+        // Could panic at any moment...
+        (&mut (&mut self.jobs.get_mut(&job).unwrap().process).stdin).as_mut().unwrap()
+    }
+    
     pub fn get_stdout(&mut self, job: usize) -> &mut ChildStdout {
         // Could panic at any moment...
         (&mut (&mut self.jobs.get_mut(&job).unwrap().process).stdout).as_mut().unwrap()
+    }
+    
+    pub fn get_stderr(&mut self, job: usize) -> &mut ChildStderr {
+        // Could panic at any moment...
+        (&mut (&mut self.jobs.get_mut(&job).unwrap().process).stderr).as_mut().unwrap()
     }
 }
