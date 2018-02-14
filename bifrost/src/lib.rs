528d3b8bc0864dcfa9f23e8255deeb8d0f846ede
SEGFAULT <fakespamtrap@gmail.com>
2018-02-14 02:33:06 +0100

    I am not tracking this, stuff is happening.

--- bifrost/src/lib.rs
+++ bifrost/src/lib.rs
@@ -1,3 +1,8 @@
+//! #Bifrost
+//! API for applications native to the CUE windowing system.
+//! Allows transparent use of cursive to intergrate into CUE.
+//! If cue is unavailable this API will create and wrap a cursive instance.
+//! 
 #[cfg(test)]
 mod tests {
     #[test]
